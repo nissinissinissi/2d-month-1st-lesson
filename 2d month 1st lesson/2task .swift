e6db7db2e7676eb81a4e5ac3f95bb8587b6b821b
nissinissinissi <nissisalamat14@icloud.com>
2022-08-28 14:38:31 -0500

    up

--- 2d month 1st lesson/2task .swift
+++ 2d month 1st lesson/2task .swift
@@ -5,29 +5,44 @@
 //  Created by User on 30 Av 5782.
 //
 
-let shop = Market (items: "CocaCola, pepsi", sweetItems: "Twix", saltyItems: "Chips")
+
 class Market {
     var SoftDrinks : String = ""
     var sweetItems: String = ""
     var saltyItems : String = ""
-    init(items:String, sweetItems:String, saltyItems:String) {
-        self.SoftDrinks = items
+    init(SoftDrinks:String, sweetItems:String, saltyItems:String) {
+        self.SoftDrinks = SoftDrinks
         self.sweetItems = sweetItems
         self.saltyItems = saltyItems
     }
     func greeting(){
-        print ("добро пожаловать в наш магазин, у нас имеется:")
+        print ("добро пожаловать в наш магазин, у нас в продаже:")
         print(" из газировок : \(SoftDrinks)")
-        print("  из сладкого: \(sweetItems)")
+        print(" из сладкого: \(sweetItems)")
         print(" из соленого: \(saltyItems)")
     }
 }
-let Items = Market (items: "CocaCola, pepsi, mirinda", sweetItems: "Twix Snickers, Kinder", saltyItems: "Chips, SaltedCaramel")
+let Items = Market (SoftDrinks: "CocaCola, pepsi, mirinda" ,  sweetItems: "Twix Snickers, Kinder", saltyItems: "Chips, SaltedCaramel")
+
 var marketArray: [Market] = []
 marketArray.append(Items)
- for market in marketArray {
-    market.greeting()
+
+for market in marketArray {
+   market.greeting()
 }
+
+ print ("добавьте продукты по желанию")
+var addItems = readLine()
+ print ("добавьте чего нибудь из газировок")
+var addSoftDrinks = readLine()
+ print ("добавьте чего нибудь из соленого")
+var addSalty = readLine()
+ print ("добавьте чего нибудь из сладкого")
+var addSweet = readLine()
+ for market in marketArray {
+   market.greeting()
+}
+
 
 
 
